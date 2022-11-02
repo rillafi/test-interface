7b287d6462f33fa438c3f82378a68302cb384c6b
aaronknudtson <aaron.knudtson@outlook.com>
2022-11-02 17:03:28 -0400

    refactored to use the multicall contract rather than individual calls

--- hooks/useDashboardFetch.ts
+++ hooks/useDashboardFetch.ts
@@ -9,47 +9,42 @@ export function useDashboardFetch() {
     const { data: balanceData, refetch: fetchBalance } = useBalance({
         addressOrName: address,
     });
-    /* const { data: readData, refetch: fetchContractReads, isFetching } = useContractReads({ */
-    /*     contracts: isLoading ? [] : [ */
-    const { data: taskClaimedTokens} = useContractRead({
-        addressOrName: contracts?.TokenClaim?.address,
-        contractInterface: contracts?.TokenClaim?.abi,
-        functionName: "taskClaimedTokens",
-        args: [address],
-        enabled: !!address && !!contracts
+    const { data: readData, refetch: fetchContractReads, isFetching } = useContractReads({
+        contracts: isLoading ? [] : [
+            {
+                addressOrName: contracts?.TokenClaim?.address,
+                contractInterface: contracts?.TokenClaim?.abi,
+                functionName: "taskClaimedTokens",
+                args: [address],
+            },
+            {
+                addressOrName: contracts?.DonationRouter?.address,
+                contractInterface: contracts?.DonationRouter?.abi,
+                functionName: "taskDonateTRilla",
+                args: [address],
+            },
+            {
+                addressOrName: contracts?.DonationRouter?.address,
+                contractInterface: contracts?.DonationRouter?.abi,
+                functionName: "taskDonateRillaUSDC",
+                args: [address],
+            },
+            {
+                addressOrName: contracts?.tRILLA?.address,
+                contractInterface: contracts?.tRILLA?.abi,
+                functionName: "allowance",
+                args: [address, contracts?.VoteEscrow?.address],
+            },
+            {
+                addressOrName: contracts?.VoteEscrow?.address,
+                contractInterface: contracts?.VoteEscrow?.abi,
+                functionName: "user_point_history__ts",
+                args: [address, 1],
+            },
+            // allTokens
+        ],
     });
-    const { data: taskDonateTRilla} = useContractRead({
-        addressOrName: contracts?.DonationRouter?.address,
-        contractInterface: contracts?.DonationRouter?.abi,
-        functionName: "taskDonateTRilla",
-        args: [address],
-        enabled: !!address && !!contracts
-    });
-    const { data: taskDonateRillaUSDC} = useContractRead({
-        addressOrName: contracts?.DonationRouter?.address,
-        contractInterface: contracts?.DonationRouter?.abi,
-        functionName: "taskDonateRillaUSDC",
-        args: [address],
-        enabled: !!address && !!contracts
-    });
-    const { data: allowance} = useContractRead({
-        addressOrName: contracts?.tRILLA?.address,
-        contractInterface: contracts?.tRILLA?.abi,
-        functionName: "allowance",
-        args: [address, contracts?.VoteEscrow?.address],
-        enabled: !!address && !!contracts
-    });
-    const { data: balanceOf} = useContractRead({
-        addressOrName: contracts?.VoteEscrow?.address,
-        contractInterface: contracts?.VoteEscrow?.abi,
-        functionName: "user_point_history__ts",
-        args: [address, 1],
-        enabled: !!address && !!contracts
-    });
-    // allTokens
-    const readData = useMemo(() => [taskClaimedTokens, taskDonateTRilla, taskDonateRillaUSDC, allowance, balanceOf], [taskClaimedTokens, taskDonateTRilla, taskDonateRillaUSDC, allowance, balanceOf]);
-    /*     ], */
-    /* }); */
+    /* const readData = useMemo(() => [taskClaimedTokens, taskDonateTRilla, taskDonateRillaUSDC, allowance, balanceOf], [taskClaimedTokens, taskDonateTRilla, taskDonateRillaUSDC, allowance, balanceOf]); */
     const refetch = useCallback(() => {
         fetchBalance();
         /* fetchContractReads(); */
